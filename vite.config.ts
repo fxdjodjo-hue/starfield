6c0bad6ea4580b94dfbc9f0b54b2e02ef9ae6e6e
fxdjodjo-hue <fxdjodjo-hue@users.noreply.github.com>
2026-01-04 15:55:44 +0100

    Temporarily remove base path for gh-pages build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/starfield/' : '/',
+  base: '/',
   server: {
     port: 5173,
   },
